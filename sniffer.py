3c0d8dc176479869ae1840c8f8fd705bfe72e6d5
Denis Krivoruchko <denis.krvrchk@gmail.com>
2022-01-13 19:35:39 +0300

    solution without scapy

--- sniffer.py
+++ sniffer.py
@@ -1,47 +1,57 @@
-from scapy.all import *
-from mac_vendor_lookup import MacLookup
-from getmac import get_mac_address
+import socket
+import binascii
+import struct
+import os
+import re
+import time
+from getpass import getuser
+from urllib.request import urlopen
+
+OUI_STORE = os.path.join(os.path.expanduser("~" + getuser()), ".oui-cache")
+CACHE_TIME = 604800
+IEEE_URL = "http://standards-oui.ieee.org/oui/oui.txt"
+BUFFER_SIZE = 65535
 
 
-MAC = get_mac_address()
+def get_vendor(mac):
+    if time.time() - os.stat(OUI_STORE).st_ctime > CACHE_TIME:
+        with open(OUI_STORE, "wb") as oui:
+            for line in urlopen(IEEE_URL).readlines():
+                oui.write(line)
+
+    formatted_mac = "-".join(mac.decode("ascii").split("-")[:3])
+    with open(OUI_STORE, "r", encoding="utf-8") as oui:
+        for line in iter(oui):
+            if re.search(formatted_mac, line, re.IGNORECASE):
+                return line.split("\t")[2].rstrip()
 
 
-def process_packet(output_format, p):
-    src_mac = p[0][0].src
-    src_vendor = MacLookup().lookup(src_mac)
+def sniff(sniff_socket, output_format):
+    raw_data, _ = sniff_socket.recvfrom(BUFFER_SIZE)
+    print(raw_data)
+    print("\n")
+    _, _, ptype = struct.unpack("!6s6sH", raw_data[:14])
 
-    dst_mac = p[0][0].dst
-    dst_vendor = MacLookup().lookup(dst_mac)
+    if socket.htons(ptype) == 1544:
+        _, _, _, _, opcode, src_mac, _, dst_mac, _ = struct.unpack("2s2s1s1s2s6s4s6s4s", raw_data[14:42])
 
-    if src_mac == MAC:
-        packet_type = "request"
-    else:
-        packet_type = "reply"
+        opcode = binascii.hexlify(opcode, "-")
+        src_mac = binascii.hexlify(src_mac, "-")
+        dst_mac = binascii.hexlify(dst_mac, "-")
 
-    print(output_format.format(packet_type, src_mac, src_vendor, dst_mac, dst_vendor))
+        print(output_format.format("request" if opcode == bytes("00-01", encoding="utf-8") else "reply",
+                                   bytes(src_mac).decode(), get_vendor(src_mac),
+                                   bytes(src_mac).decode(), get_vendor(dst_mac)))
 
 
 def main():
-    type_format = "{:<15}"
-    mac_format = "{:<30}"
-    vendor_format = "{:<30}"
-    output_format = f"{type_format} {mac_format} {vendor_format} {mac_format} {vendor_format}"
+    sniff_socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
 
+    output_format = "{:<15} {:<30} {:<30} {:<30} {:<30}"
     print(output_format.format("Packet type", "Source MAC", "MAC Vendor", "Destination MAC", "MAC Vendor"))
-
     while True:
-        try:
-            sniff(filter="arp", prn=lambda p: process_packet(output_format, p), count=10)
-        except Exception:
-            print("Something went wrong.")
-
-        inp = input("Do you want to continue? (y/n) \n")
-        while inp != "y" and inp != "n":
-            inp = input("Do you want to continue? (y/n) \n")
-
-        if inp == "n":
-            break
+        sniff(sniff_socket, output_format)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
